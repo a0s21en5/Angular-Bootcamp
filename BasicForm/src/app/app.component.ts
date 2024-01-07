fd3b929c6fc0910e84bbe1246a72d6fe09db60a2
Ashish Sahu <ashsahu9713@gmail.com>
2024-01-07 21:12:05 +0530

    Add Basic Form

---
+++ BasicForm/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'BasicForm';
+
+  userData: any = {}
+  getData(data: NgForm) {
+    console.warn(data)
+    this.userData = data
+  }
+}
