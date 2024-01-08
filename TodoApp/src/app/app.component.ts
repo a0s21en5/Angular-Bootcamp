b4e3c128a04a807dd6080c252b1180d0883afeb3
Ashish Sahu <ashsahu9713@gmail.com>
2024-01-08 23:55:39 +0530

    Add Todo Web App

---
+++ TodoApp/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TodoApp';
+
+  TaskList: any[] = []
+
+  AddTask(item: string) {
+    this.TaskList.push({
+      id: this.TaskList.length,
+      name: item
+    })
+    console.warn(this.TaskList)
+  }
+
+  RemoveTask(id: number) {
+    this.TaskList=this.TaskList.filter(item=>item.id!==id)
+  }
+}
